8ac4f6187f8ee08c514456a77d006b507542317f
Davide Amato <davide.amato@colorado.edu>
2018-03-28 15:45:13 -0700

    Add description of 'out' in input.txt

--- dp/settings.f90
+++ dp/settings.f90
@@ -65,7 +65,7 @@ read(id_set,'(a11,e22.15)') dummy, tol
 read(id_set,'(a11,e22.15)') dummy, tspan
 read(id_set,'(a11,e22.15)') dummy, tstep
 read(id_set,'(a11,e10.1,5(/))') dummy, rmxstep
-read(id_set,'(a11,i3,3(/))') dummy, eqs
+read(id_set,'(a11,i3,4(/))') dummy, eqs
 read(id_set,'(a11,i3)') dummy, verb
 read(id_set,'(a4,a)') dummy,outpath
 
