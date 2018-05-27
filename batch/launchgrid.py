cb908902280423022b56bde2e370949068c32173
Davide Amato <davide.amato@colorado.edu>
2018-05-26 18:31:18 -0700

    Launch grid python file template

---
+++ batch/launchgrid.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""
+
+
+"""
+  
+
+
+
+def main():
+
+  # Get number of simulations and trajectory structure
+  
+  # Output to user: number of simulations, rough estimate of CPU time and memory
+
+  # Map the grid of initial conditions onto the THALASSA executable
+
+  # Start from SID = 1 (default) or a given SID.
+
+  # Save diagnostics to log file.
+
+
+  
+  
+
+if __name__ == '__main__':
+  main()
